1e9f891024d966bfacecf570e7544be27aeac535
gyhou <47550192+gyhou@users.noreply.github.com>
2019-09-22 14:00:46 -0500

    Add files via upload

--- green_foot_app.py
+++ green_foot_app.py
@@ -25,7 +25,7 @@ def address():
     # coords_2 = inputs['coords_2']
     car_distance_km = inputs['car_distance']
     car_distance_miles = car_distance_km * 0.621
-    # bus_distance_miles = inputs['bus_distance']
+    bus_distance_miles = inputs['bus_distance']
     # train_distance_miles = inputs['train_distance']
     # if inputs['passengers'] < 1:
     #     passengers = 1
@@ -44,7 +44,7 @@ def address():
     # Emission rate
     co2e_lbs_per_gas_gal = 19.830
     co2e_lbs_per_diesel_gal = 22.716
-    # bus_co2_lbs_per_passenger_mile = .17 / .988  # CO2 equivalents conversion
+    bus_co2e_lbs_per_passenger_mile = .13 / .988  # CO2 equivalents conversion
     # train_co2e_lbs_per_passenger_mile = .31
     # flight_co2e_lbs_per_passenger_mile = .39
 
@@ -61,15 +61,15 @@ def address():
         co2e_lbs_per_gas_gal / avg_car_mpg
     car_diesel_co2e_emission_per_vehicle = car_distance_miles * \
         co2e_lbs_per_diesel_gal / avg_car_mpg
-    # bus_emission_per_passenger = bus_co2_lbs_per_passenger_mile * bus_distance_miles
+    bus_emission_per_passenger = bus_co2e_lbs_per_passenger_mile * bus_distance_miles
     # train_emission_per_passenger = train_co2e_lbs_per_passenger_mile * train_distance_miles
     # flight_emission_per_passenger = flight_co2e_lbs_per_passenger_mile * flight_distance_miles * rf_factor
 
     # Use a dictionary to format output for json
     out = {'Pounds of CO2E emission per car on gasoline (22 mpg)': car_gas_co2e_emission_per_vehicle,
-           'Pounds of CO2E emission per car on diesel (22 mpg)': car_diesel_co2e_emission_per_vehicle} #,
-        #    'Pounds of CO2E emission per passenger on bus': bus_emission_per_passenger,
-        #    'Pounds of CO2E emission per passenger on train': train_emission_per_passenger}
+           'Pounds of CO2E emission per car on diesel (22 mpg)': car_diesel_co2e_emission_per_vehicle,
+           'Pounds of CO2E emission per passenger on bus': bus_emission_per_passenger}  # ,
+    #    'Pounds of CO2E emission per passenger on train': train_emission_per_passenger}
 
     # give output to sender.
     return app.response_class(
